516e5993a8e256a322a251d6eec0e3f95042e79f
Matthias Jansen <jansen@clarifydata.de>
2020-03-23 15:27:19 +0100

    use single method

--- src/get_market_share.py
+++ src/get_market_share.py
@@ -6,17 +6,8 @@ import requests
 BASE_URL = "http://127.0.0.1:5000/api/v1/plz_to_market_share"
 
 
-def get_market_share_category(category, plz_with_cust_amounts):
-    url = f"{BASE_URL}/{category}"
-    data = json.dumps(plz_with_cust_amounts)
-    response = requests.post(
-        url, data=data, headers={"content-type": "application/json"}
-    )
-    return json.loads(response.content)
-
-
-def get_market_share_sparte(sparte, plz_with_cust_amounts):
-    url = f"{BASE_URL}/{sparte}"
+def get_market_share(type, plz_with_cust_amounts):
+    url = f"{BASE_URL}/{type}"
     data = json.dumps(plz_with_cust_amounts)
     response = requests.post(
         url, data=data, headers={"content-type": "application/json"}
