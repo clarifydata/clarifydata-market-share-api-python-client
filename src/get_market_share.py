64d9da132a4db359defb0ec9c7add2b4789f76f3
Matthias Jansen <jansen@clarifydata.de>
2020-03-23 15:45:58 +0100

    rename method

--- src/get_market_share.py
+++ src/get_market_share.py
@@ -6,7 +6,7 @@ import requests
 BASE_URL = "http://127.0.0.1:5000/v1/plz_to_market_share"
 
 
-def get_market_share(type, plz_with_cust_amounts):
+def get_market_shares(type, plz_with_cust_amounts):
     url = f"{BASE_URL}/{type}"
     data = json.dumps(plz_with_cust_amounts)
     response = requests.post(
