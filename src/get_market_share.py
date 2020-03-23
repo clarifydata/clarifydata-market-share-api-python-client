2a1d16522c55fe4d87f69dc955f7e7c933adef74
Matthias Jansen <jansen@clarifydata.de>
2020-03-23 15:25:37 +0100

    add pipfiles. refacroting

--- src/get_market_share.py
+++ src/get_market_share.py
@@ -3,8 +3,20 @@ import json
 import requests
 
 
-def get_market_share_from_api(type, plz_with_cust_amounts):
-    url = f"http://127.0.0.1:5000/api/v1/plz_to_market_share/{type}"
+BASE_URL = "http://127.0.0.1:5000/api/v1/plz_to_market_share"
+
+
+def get_market_share_category(category, plz_with_cust_amounts):
+    url = f"{BASE_URL}/{category}"
+    data = json.dumps(plz_with_cust_amounts)
+    response = requests.post(
+        url, data=data, headers={"content-type": "application/json"}
+    )
+    return json.loads(response.content)
+
+
+def get_market_share_sparte(sparte, plz_with_cust_amounts):
+    url = f"{BASE_URL}/{sparte}"
     data = json.dumps(plz_with_cust_amounts)
     response = requests.post(
         url, data=data, headers={"content-type": "application/json"}
