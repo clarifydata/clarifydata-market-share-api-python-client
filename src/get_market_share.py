a2a7bdd88b135ce9a6010746758636630b48830a
Matthias Jansen <jansen@clarifydata.de>
2020-03-23 15:31:29 +0100

    use v1 base path

--- src/get_market_share.py
+++ src/get_market_share.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 
-BASE_URL = "http://127.0.0.1:5000/api/v1/plz_to_market_share"
+BASE_URL = "http://127.0.0.1:5000/v1/plz_to_market_share"
 
 
 def get_market_share(type, plz_with_cust_amounts):
