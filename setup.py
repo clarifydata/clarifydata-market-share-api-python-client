ae462816ca9239835aaebcb4ced459f99aa4fec9
Matthias Jansen <jansen@clarifydata.de>
2020-03-25 16:00:55 +0100

    add pipenv-setup and sync setup.py with pipfiles

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ setup(
     version="0.1",
     url="https://github.com/clarifydata/plz_service_api_wrapper",
     packages=["plzwrapper"],
-    install_requires=["requests"],
+    install_requires=[
+        "certifi==2019.11.28",
+        "chardet==3.0.4",
+        "idna==2.9",
+        "requests==2.23.0",
+        "urllib3==1.25.8",
+    ],
 )
