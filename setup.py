4fa68a929a3f05bd680647dc017fd30ef91061b2
Matthias Jansen <jansen@clarifydata.de>
2020-03-25 09:42:53 +0100

    add more metadata

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     name="plzwrapper",
+    version="0.1",
+    url="https://github.com/clarifydata/plz_service_api_wrapper",
     packages=["plzwrapper"],
     install_requires=["requests"],
 )
