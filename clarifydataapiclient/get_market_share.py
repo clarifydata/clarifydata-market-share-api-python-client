c7030bca57a09ecdb075c1b717b07ba7c7a1164b
Matthias Jansen <jansen@clarifydata.de>
2020-04-03 09:01:18 +0200

    rename variable to avoid overriding internal type keyword

--- clarifydataapiclient/get_market_share.py
+++ clarifydataapiclient/get_market_share.py
@@ -13,7 +13,7 @@ def get_market_shares(
     """
     Get market share for different categories (Strom, Gas, ...)
     and "Sparten" (Energie, Tel)
-    :param type: Strom, Gas, Energie, FN (Festnetz), MF (Mobilfunk), Tel
+    :param sparte_or_category: Strom, Gas, Energie, FN (Festnetz), MF (Mobilfunk), Tel
     :param plz_with_cust_amounts: Dictionary containing PLZ
     as keys and corresponding customer amounts as values
     :param gfk_weight: weight of gfk data in calculated results
@@ -21,7 +21,7 @@ def get_market_shares(
     :param version: specify which version to use
     :return: dictionary with PLZ as keys and calculated market share as values
     """
-    url = f"{base_path}/{version}/'market_share'/{type}"
+    url = f"{base_path}/{version}/'market_share'/{sparte_or_category}"
     if gfk_weight != 0:
         url += f"?gfk_weight={gfk_weight}"
     data = json.dumps(plz_with_cust_amounts)
