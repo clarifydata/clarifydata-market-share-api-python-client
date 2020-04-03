3817e04f19b4665c4d1677acfc2dddaee4454900
Matthias Jansen <jansen@clarifydata.de>
2020-04-03 09:32:54 +0200

    small fixes

--- cdmarketshares/get_market_share.py
+++ cdmarketshares/get_market_share.py
@@ -8,7 +8,7 @@ def get_market_shares(
     plz_with_cust_amounts,
     gfk_weight=0,
     base_path="http://market-share.clarifydata.de",
-    version="1.0",
+    version="v1",
 ):
     """
     Get market share for different categories (Strom, Gas, ...)
@@ -21,7 +21,7 @@ def get_market_shares(
     :param version: specify which version to use
     :return: dictionary with PLZ as keys and calculated market share as values
     """
-    url = f"{base_path}/{version}/'market_share'/{sparte_or_category}"
+    url = f"{base_path}/{version}/market_share/{sparte_or_category}"
     if gfk_weight != 0:
         url += f"?gfk_weight={gfk_weight}"
     data = json.dumps(plz_with_cust_amounts)
