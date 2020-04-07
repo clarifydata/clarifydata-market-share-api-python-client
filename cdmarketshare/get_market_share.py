3982dd61849325e1c57a73c148b8d3778f89d6a4
Matthias Jansen <jansen@clarifydata.de>
2020-04-07 07:30:19 +0200

    use postcode naming

--- cdmarketshare/get_market_share.py
+++ cdmarketshare/get_market_share.py
@@ -5,7 +5,7 @@ import requests
 
 def get_market_share(
     sparte_or_category,
-    plz_with_cust_amounts,
+    postcodes_with_cust_amounts,
     gfk_weight=0,
     host="market-share.clarifydata.de",
     version="v1",
@@ -14,7 +14,7 @@ def get_market_share(
     Get market share for different categories (Strom, Gas, ...)
     and "Sparten" (Energie, Tel)
     :param sparte_or_category: Strom, Gas, Energie, FN (Festnetz), MF (Mobilfunk), Tel
-    :param plz_with_cust_amounts: Dictionary containing PLZ
+    :param postcodes_with_cust_amounts: Dictionary containing postcodes
     as keys and corresponding customer amounts as values
     :param gfk_weight: weight of gfk data in calculated results
     :param host: path to the API
@@ -24,7 +24,7 @@ def get_market_share(
     url = f"http://{host}/{version}/market_share/{sparte_or_category}"
     if gfk_weight != 0:
         url += f"?gfk_weight={gfk_weight}"
-    data = json.dumps(plz_with_cust_amounts)
+    data = json.dumps(postcodes_with_cust_amounts)
     response = requests.post(
         url, data=data, headers={"content-type": "application/json"}
     )
